587e25edcf5587a079c02beee1674d1081b2fdfc
AgusQuintanar <agusquintanar17@gmail.com>
2020-04-24 10:18:56 -0500

    solved cf266

--- ICPC/CF-A/CF431-D2-A.cpp
+++ ICPC/CF-A/CF431-D2-A.cpp
@@ -7,24 +7,22 @@
 #define debug(x) 42
 #define debug_c(a) 42
 #endif
+
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-
-	int n; cin >> n;
-	int repitedChars = 0;
+	int calorieCount = 0;
+	vector<int> cals (4);
+	for (int i=0; i<4; i++) cin >> cals[i];
 	string s; cin >> s;
 
-	for (int i=0; i<n-1; i++) {
-		if (s[i+1] == s[i]) repitedChars++;
-	}
-
-	cout << repitedChars << endl;
-
+	for (char& c : s) calorieCount += cals[(short)c - 48 - 1];
+	cout << calorieCount << endl;
 }
 
-//92 ms	0 KB
+// 31 ms	300 KB
