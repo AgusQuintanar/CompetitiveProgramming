ff96b299313c2dec0d39bd3988749d68f639a324
AgusQuintanar <agusquintanar17@gmail.com>
2020-06-22 21:34:32 -0500

    solved cf766

---
+++ ICPC/CF-A/CF766-D2-A.cpp
@@ -0,0 +1,30 @@
+#ifdef LOCAL
+#include <iostream>
+#define debug(x) cerr<<"["<<#x<<"]: "<<x<<endl;
+#define EPS 0.0000001
+#else
+#include <bits/stdc++.h>
+#define debug(x) 42
+#define debug_c(a) 42
+#endif
+
+#include <string>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+    int result;
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    if (str1 == str2) result = -1;
+    else if (str1.length() > str2.length()) result = str1.length();
+    else result = str2.length();
+
+    cout << result << endl;
+   
+}
+
